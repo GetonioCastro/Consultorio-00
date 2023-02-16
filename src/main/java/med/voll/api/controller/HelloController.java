a06b3824b8a3ba7cce9717a2075bd1f62b5ad7dd
Getonio Castro <102559370+GetonioCastro@users.noreply.github.com>
2023-02-15 21:35:50 -0300

    implementando o cadastro de medicos usando DTO com Java Record

--- src/main/java/med/voll/api/controller/HelloController.java
+++ src/main/java/med/voll/api/controller/HelloController.java
@@ -1,17 +1,17 @@
-package med.voll.api.controller;
+package med.voll.api.controller; //<- O .controller cria uma subclass para organizar melhor o projeto
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/hello")
+@RestController            //Anotação para RestController
+@RequestMapping("/hello") //Anotação para mapeamento da requisição
 public class HelloController {
-    @GetMapping
+    @GetMapping             //se for uma requisição do tipo GET chama o metodo olaMundo()
     public String olaMundo(){
         //retorna o Hello World no end: localhost:8080/hello
-        return "Hello World! GETONIO Castro";
+        return "Hello World! GETONIO CASTRO";
     }
 
 }
