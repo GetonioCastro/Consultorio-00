e59f61344d2728c3cd058240f3ec3b84e6291617
Getonio Castro <102559370+GetonioCastro@users.noreply.github.com>
2023-02-17 00:48:57 -0300

    implementando o cadastro de pacietens

--- src/main/java/med/voll/api/paciente/DadosCadastroPaciente.java
+++ src/main/java/med/voll/api/paciente/DadosCadastroPaciente.java
@@ -1,2 +1,7 @@
-package med.voll.api.controller;public record DadosCadastroPaciente() {
+package med.voll.api.paciente;
+
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosCadastroPaciente(String nome, String email, String telefone, DadosEndereco endereco) {
 }
+
